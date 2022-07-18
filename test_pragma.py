dbc4252035d7047aa16d5af0338cd9ca2ff6d571
georgiy <M0r3r0b0t1355c10n355>
2022-07-18 22:19:37 +0300

    next_test

--- test_pragma.py
+++ test_pragma.py
@@ -1,4 +1,4 @@
-
+import time
 
 HOME = '//*[@title="Home"]'
 CLIENT_HUB = '//*[@id="menu-item-23582"]//*[contains(text(),"Client Hub")]'
@@ -9,6 +9,17 @@ BINGO = '//*[@id="menu-item-11081"]//*[contains(text(),"Bingo")]'
 
 ORANGE_COLOR = 'rgb(255, 128, 14)'
 WHITE_COLOR = 'rgb(255, 255, 255)'
+
+JPT_ROOM = '//*[@class="slick-slide slick-current slick-active"]//*[contains(text(), "The Jackpot Room")]'
+BNG_BLAST = '//*[@class="slick-slide slick-current slick-active"]//*[contains(text(), "Bingo Blast")]'
+REELS_ROOM = '//*[@class="slick-slide slick-current slick-active"]//*[contains(text(), "Reels Room")]'
+CNTR_ROADS = '//*[@class="slick-slide slick-current slick-active"]//*[contains(text(), "Country Roads")]'
+ZM_ROOM = '//*[@class="slick-slide slick-current slick-active"]//*[contains(text(), "Zoom Room")]'
+BOOMBOX = 'body > div.gp__content > section > div.games > div.container > div.live-bingo.row.slick-initialized.slick-slider > div.slick-list.draggable > div > div.slick-slide.slick-current.slick-active > div > div > p'
+SW_BANABZA_BNGO = '//*[@class="slick-slide slick-current slick-active"]//*[contains(text(), "Sweet Bonanza Bingo")]'
+SNWBALL_BLAST = '//*[@class="slick-slide slick-current slick-active"]//*[contains(text(), "Snowball Blast")]'
+ROCK_N_SWING = '//*[@class="slick-slide slick-current slick-active"]//*[contains(text(), "Rock N Swing")]'
+RIGHT_ARROW = '//*[@class="right-arrow slick-arrow"]'
 
 
 def test_example_is_working(page):
@@ -36,3 +47,37 @@ def test_example_is_working(page):
     color_bingo_btn = bingo_btn.evaluate("(element) => window.getComputedStyle(element).color")
     assert color_bingo_btn == ORANGE_COLOR
 
+
+def test_second(page):
+    page.goto("https://www.pragmaticplay.com/en")
+    page.locator("text=Yes, I am 18 years or older").click()
+    page.locator(PRODUCTS).click()
+    bingo_btn = page.locator(BINGO)
+    bingo_btn.click()
+    time.sleep(1)
+    page.locator(JPT_ROOM).hover()
+    page.locator(RIGHT_ARROW).click()
+    time.sleep(1)
+    page.locator(BNG_BLAST).hover()
+    page.locator(RIGHT_ARROW).click()
+    time.sleep(1)
+    page.locator(REELS_ROOM).hover()
+    page.locator(RIGHT_ARROW).click()
+    time.sleep(1)
+    page.locator(CNTR_ROADS).hover()
+    page.locator(RIGHT_ARROW).click()
+    time.sleep(1)
+    page.locator(ZM_ROOM).hover()
+    page.locator(RIGHT_ARROW).click()
+    time.sleep(1)
+    page.locator(BOOMBOX).hover()
+    page.locator(RIGHT_ARROW).click()
+    time.sleep(1)
+    page.locator(SW_BANABZA_BNGO).hover()
+    page.locator(RIGHT_ARROW).click()
+    time.sleep(1)
+    page.locator(SNWBALL_BLAST).hover()
+    page.locator(RIGHT_ARROW).click()
+    time.sleep(1)
+    page.locator(ROCK_N_SWING).hover()
+    page.locator(RIGHT_ARROW).click()
